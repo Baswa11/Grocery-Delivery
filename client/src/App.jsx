776b890efe1165f09c10554e09d666f9e22cc9fa
baswa11 <baswa11@users.noreply.github.com>
2026-01-13 09:32:56 +0530

    modified:   client/src/App.jsx 	modified:   client/src/components/Navbar.jsx 	new file:   client/src/components/mainBanner.jsx 	modified:   client/src/context/AppContext.jsx 	modified:   client/src/main.jsx 	new file:   client/src/pages/Allproducts.jsx 	new file:   client/src/pages/Contact.jsx 	new file:   client/src/pages/Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Allproducts from './pages/Allproducts'
+import Contact from './pages/Contact'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar />
+      {isSellerPath ? null : <Navbar />}
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/allproducts" element={<Allproducts />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   )
 }
 
-export default App  
+export default App
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,18 +1,35 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-const AppContextProvider = (props) => {
+const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+
 
     const value = {
-
+        user,
+        setUser,
+        isSeller,
+        setIsSeller,
+        showUserLogin,
+        setShowUserLogin,
+        navigate
     }
 
     return (
         <AppContext.Provider value={value}>
-            {props.children}
+            {children}
         </AppContext.Provider>
     )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
 }
 
 export default AppContextProvider;
